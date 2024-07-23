e3a90eb7c9ae66b6eaeaa4c44ba3192086670941
Nyankoye Daniel FELEMOU <nyankoyedanielfelemou32156@gmail.com>
2024-07-23 22:33:14 +0000

    Jour 11 : Deux clefs du type de relation eloquent (hasMany et belongsTo)

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Employer;
 
 class Job extends Model
 {
@@ -11,4 +12,9 @@ class Job extends Model
 
     protected $table = 'job_listings';
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
